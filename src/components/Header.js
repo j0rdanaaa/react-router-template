7d3b4acb282586737fc82420a62e1b0dbed33773
j0rdanaaa <j0rdanaaa@gmail.com>
2023-08-19 09:40:33 -0300

    router template

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
-import {StyledHeader, StyledButton} from "../style"
+import { useNavigate } from "react-router-dom";
+import { StyledHeader, StyledButton } from "../style";
+import { goToProfile, goToHome } from "../routes/coordinator";
 
 function Header() {
+  const navigate = useNavigate();
+  
 
+  
   return (
     <StyledHeader>
-      <StyledButton >
+      <StyledButton onClick={() => goToHome(navigate)}>
         Ir para página inicial
       </StyledButton>
-      <StyledButton >
+
+      <StyledButton onClick={() => goToProfile(navigate, "easley")}>
         Ir para página de perfil
       </StyledButton>
     </StyledHeader>
---
+++ src/routes/coordinator.js
@@ -0,0 +1,10 @@
+
+
+
+export const goToHome = (navigate) => {
+    navigate("/");
+  };
+
+export const goToProfile = (navigate, name) => {
+    navigate(`/profile/${name}`);
+  };
